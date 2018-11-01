afdb0ae02ddf861f105b2dee876dda309e89fcfc
albergg <alber.gonzalez.g@gmail.com>
2018-11-01 01:10:40 -0300

    database.php creada

---
+++ Classes/Databases/Database.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace VG\Databases;
+
+interface Database {
+
+    public function connect();
+    
+    public function insert(array $data);
+
+    // public function update();
+
+    // public function delete();
+
+    public function fetch(array $condition);
+
+    public function fetchAll();
+}
